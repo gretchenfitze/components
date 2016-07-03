5b72fe2ed4f1d0d18aef7d9cb62333badf3ffea0
gretchenfitze <gretchenfitze@gmail.com>
2016-07-03 22:23:35 +0300

    Add delete button and improve UI

--- menu.js
+++ menu.js
@@ -1,8 +1,10 @@
 class Menu {
 	constructor(options) {
 		this.el = options.el;
+		this.items = document.querySelectorAll('.menu__item');
 
 		this._initEvents();
+		this._addRemoveButtons();
 	}
 
 	_initEvents () {
@@ -12,20 +14,34 @@ class Menu {
 
 	_onMenuClick (event) {
 		let isItemClick = false;
-		if (event.target.classList.contains('menu__item')) {
+		if (event.target.classList.contains('menu__remove-button')) {
+			isItemClick = true;
+			this._deleteElement(event);
+		} else if (event.target.classList.contains('menu__item')) {
 			isItemClick = true;
 			this._onMenuItemClick(event);
 		}
 
 		if (!isItemClick) {
 			this.el.classList.toggle('menu_open');
-		} 
-		
+		}
 	}
-
 
 	_onMenuItemClick (event) {
 		console.log(event.target);
+	}
+
+	_addRemoveButtons() {
+		Array.prototype.forEach.call(this.items, function(item) {
+			let removeButton = document.createElement('button');
+			removeButton.classList.add('menu__remove-button');
+			removeButton.innerHTML = '✖';
+			item.appendChild(removeButton);
+		})
+	}
+
+	_deleteElement (event) {
+		event.target.parentNode.parentNode.removeChild(event.target.parentNode);
 	}
 
 }
