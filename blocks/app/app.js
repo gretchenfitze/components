0115e00c7903238566d63ed5761f530c96845fc4
Margarita Klubochkina <gretchenfitze@gmail.com>
2016-07-09 02:59:57 +0300

    Get data from JSON by XMLHTTPRequest

--- blocks/app/app.js
+++ blocks/app/app.js
@@ -4,6 +4,11 @@
   // Import Menu constructor
 	let Menu = window.Menu;
 
+	/**
+	 * Set Menu listeners for adding and deleting items
+	 *
+	 * @param  {HTMLElement}
+	 */
 	function activateMenuListeners(menu) {
 		menu.el.addEventListener('remove', function (event) {
 			menu.removeItem(event.detail);
@@ -16,60 +21,36 @@
 		window.menu = menu;
 	}
 
+	/**
+	 * Send XMLHttpRequest to get items for new Menu
+	 *
+	 * @param  {String}
+	 */
+	function sendDataRequest(path) {
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', path, false);
+		xhr.send();
+		if (xhr.status != 200) {
+			alert( xhr.status + ': ' + xhr.statusText );
+		}
+		return JSON.parse(xhr.responseText);
+	}
+
+
+	// Create first menu
 	let menuList = new Menu({
 		el: document.querySelector('.js-menu-list'),
 		template: '#menu',
-		data: {
-			title: 'Список покупок',
-			items: [
-				{
-					name: 'Хлеб',
-					quantity: '2 батона'
-				},
-				{
-					name: 'Молоко',
-					quantity: '1 л'
-				},
-				{
-					name: 'Сыр',
-					quantity: '300 г'
-				},
-			]
-		}
+		data: sendDataRequest('data/shopping-list.json')
 	});
-
 	activateMenuListeners(menuList);
 
+	// Create second menu
 	let menuRecipe = new Menu({
 		el: document.querySelector('.js-menu-recipe'),
 		template: '#menu',
-		data: {
-			title: 'Рецепт',
-			items: [
-				{
-					name: 'Творог',
-					quantity: '350 г'
-				},
-				{
-					name: 'Яйцо куриное',
-					quantity: '2 штуки'
-				},
-				{
-					name: 'Пшеничная мука',
-					quantity: '6 столовых ложек'
-				},
-				{
-					name: 'Сахар',
-					quantity: '2 столовые ложки'
-				},
-				{
-					name: 'Подсолнечное масло',
-					quantity: '5 столовых ложек'
-				},
-			]
-		}
+		data: sendDataRequest('data/recipe.json')
 	});
-
 	activateMenuListeners(menuRecipe);
 
 })();
